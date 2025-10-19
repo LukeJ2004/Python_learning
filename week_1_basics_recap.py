e1546ff1af3a11b082937fcc009cc20b544614a6
LukeJ2004 <108278321+LukeJ2004@users.noreply.github.com>
2025-10-20 00:27:25 +0200

    Add files via upload
    This was just a quick recap of if, elif and else statements.

---
+++ week_1_basics_recap.py
@@ -0,0 +1,15 @@
+# My name is Lukhanyiso Jikela
+# This is just a recap of python basics
+# 20 October 2025
+# Write code that prints Hello if 1 is stored in spam, 
+# prints Howdy if 2 is stored in spam, and prints Greetings! 
+# if anything else is stored in spam.
+
+spam = 5
+if spam == 1:
+    print("Hello")
+elif spam == 2:
+    print("Howdy")
+else:
+    print("Greetings!")
+
